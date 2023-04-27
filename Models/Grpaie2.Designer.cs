d31f221e0879f98e7594062176065bb926daed99
Daoud Mohamed Tijani <mohamed.daoud99@hotmail.com>
2023-04-27 17:54:39 +0100

    xxxxxxxxxxxxxxxxxxx

--- Models/Grpaie2.Designer.cs
+++ Models/Grpaie2.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La génération de code T4 est activée pour le modèle 'F:\2ingeneur\StagePfa\GestiondePaieBackendPFA\Models\Grpaie2.edmx'. 
+﻿// La génération de code T4 est activée pour le modèle 'D:\2eme Génie info\PFA\version2\demo3\GestiondePaieBackendPFA\Models\Grpaie2.edmx'. 
 // Pour activer la génération de code héritée, définissez la valeur de la propriété
 // du concepteur 'Stratégie de génération de code' sur 'ObjectContext hérité'. Cette propriété est disponible dans la fenêtre Propriétés lorsque le modèle
 //  est ouvert dans le concepteur.
